4412dd87493edc147b70dcdf185c9a56b8f456c0
franius8 <113021463+franius8@users.noreply.github.com>
2022-09-20 21:05:04 +0200

    Create grid making and reset functions

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-const gridDiv = document.createElement('div');
 const containerDiv = document.querySelector('#container');
 const button = document.querySelector('#makeGrid');
+const resetButton = document.querySelector('#reset');
+
+button.addEventListener('click', makeGridFunction);
+resetButton.addEventListener('click', resetGridFunction);
+
+function makeGridFunction() {
+    console.log('Making grid');
+    for(i = 0; i < 100; i++) {
+       let gridDiv = document.createElement('div');
+       gridDiv.classList.add('gridDiv');
+       containerDiv.appendChild(gridDiv);
+    }
+    let gridNodeList = document.querySelectorAll('.gridDiv');
+    gridNodeList.forEach(div => {
+        div.addEventListener('mouseover', changeDivColor)
+    });
+}
+
+function resetGridFunction () {
+    let gridNodeList = document.querySelectorAll('.gridDiv');
+    gridNodeList.forEach(div => {
+        div.style.backgroundColor = 'lightblue';
+    });
+}
+
+function changeDivColor () {
+    this.style.backgroundColor = 'blue';
+}
